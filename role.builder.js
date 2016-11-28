9088c2a615753e0a19341a586246d5987ac298a5
Matthew Piziak <matthew.piziak@gmail.com>
2016-11-27 21:54:24 -0500

    fix roles

--- role.builder.js
+++ role.builder.js
@@ -2,28 +2,29 @@ var roadUtils = require('utils.road');
 
 var roleBuilder = {
     run: function(creep) {
-	var Action = {
-	    BUILDING: 1,
-	    UPGRADING: 2,
-	    CHARGING: 3
-	};
-	var num_harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-	if (num_harvesters.length < 3) {
-	    creep.memory.role = 'harvester';
-	    return;
-	}
-	Game.rooms[creep.pos.roomName].createConstructionSite(creep.pos.x, creep.pos.y, STRUCTURE_ROAD);
+        var Action = {
+            BUILDING: 1,
+            UPGRADING: 2,
+            CHARGING: 3
+        };
+        var num_harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+        if (num_harvesters.length < 3) {
+            creep.memory.role = 'harvester';
+            return;
+        }
+        Game.rooms[creep.pos.roomName].createConstructionSite(creep.pos.x, creep.pos.y, STRUCTURE_ROAD);
         if (creep.carry.energy == 0) {
-	    creep.memory.action = Action.CHARGING;
+            creep.memory.action = Action.CHARGING;
             creep.say('charging');
         }
-        if (!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
-	    creep.memory.action = Action.BUILDING;
-            creep.say('building');
-        }
-        if (!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity && creep.room.find(FIND_CONSTRUCTION_SITES).length == 0) {
-	    creep.memory.action = Action.UPGRADING;
-            creep.say('upgrading');
+        if (creep.carry.energy == creep.carryCapacity) {
+            if (creep.room.find(FIND_CONSTRUCTION_SITES).length == 0) {
+                creep.memory.action = Action.UPGRADING;
+                creep.say('upgrading');
+            } else {
+                creep.memory.action = Action.BUILDING;
+                creep.say('building');
+            }
         }
         if (creep.memory.action == Action.BUILDING) {
             var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
@@ -42,10 +43,10 @@ var roleBuilder = {
                 if (spawn.transferEnergy(creep, creep.carryCapacity) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(spawn);
                 } else {
-		    if (creep.body.length > 3){
-			spawn.renewCreep(creep);
-		    }
-		}
+                    if (creep.body.length > 3) {
+                        spawn.renewCreep(creep);
+                    }
+                }
             }
         }
     }
