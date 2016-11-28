d051b1b8d8b58adc8026dc694f3d66d2c6e580e9
Matthew Piziak <matthew.piziak@gmail.com>
2016-11-27 21:45:50 -0500

    make use of enums for builder-upgraders

--- role.builder.js
+++ role.builder.js
@@ -2,6 +2,11 @@ var roadUtils = require('utils.road');
 
 var roleBuilder = {
     run: function(creep) {
+	var Action = {
+	    BUILDING: 1,
+	    UPGRADING: 2,
+	    CHARGING: 3
+	};
 	var num_harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
 	if (num_harvesters.length < 3) {
 	    creep.memory.role = 'harvester';
@@ -9,32 +14,29 @@ var roleBuilder = {
 	}
 	Game.rooms[creep.pos.roomName].createConstructionSite(creep.pos.x, creep.pos.y, STRUCTURE_ROAD);
         if (creep.carry.energy == 0) {
-            creep.memory.building = false;
-            creep.memory.upgrading = false;
+	    creep.memory.action = Action.CHARGING;
             creep.say('charging');
         }
         if (!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
-            creep.memory.building = true;
-            creep.memory.upgrading = false;
+	    creep.memory.action = Action.BUILDING;
             creep.say('building');
         }
         if (!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity && creep.room.find(FIND_CONSTRUCTION_SITES).length == 0) {
-            creep.memory.building = false;
-            creep.memory.upgrading = true;
+	    creep.memory.action = Action.UPGRADING;
             creep.say('upgrading');
         }
-        if (creep.memory.building) {
+        if (creep.memory.action == Action.BUILDING) {
             var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
             if (targets.length) {
                 if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
                 }
             }
-        } else if (creep.memory.upgrading) {
+        } else if (creep.memory.action == Action.UPGRADING) {
             if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller);
             }
-        } else { // charging
+        } else {
             var spawn = Game.spawns['Hejmo'];
             if (spawn.energy > 0) {
                 if (spawn.transferEnergy(creep, creep.carryCapacity) == ERR_NOT_IN_RANGE) {
